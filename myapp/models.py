ac2ed7bc989fabe1611776440c78d3169541055e
Mario Garcia <mariogarcia.ar@gmail.com>
2024-03-02 02:28:02 -0300

    Add Task model into myapp

--- myapp/models.py
+++ myapp/models.py
@@ -3,3 +3,8 @@ from django.db import models
 # Create your models here.
 class Project(models.Model):
     name = models.CharField(max_length=200)
+
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
